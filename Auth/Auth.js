22551feb60461dac1ab65a67556782c33ea0e263
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-05-08 06:23:45 +0100

    User Update Functionality Added

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -45,6 +45,43 @@ exports.register = async (req, res, next) => {
       })
     }
   }
+
+  
+  exports.update = async (req, res, next) => {
+    const { role, id } = req.body;
+    // First - Verifying if role and id is presnt
+    if (role && id) {
+      // Second - Verifying if the value of role is admin
+      if (role === "admin") {
+        // Finds the user with the id
+        await User.findById(id)
+          .then((user) => {
+            // Third - Verifies the user is not an admin
+            if (user.role !== "admin") {
+              user.role = role;
+              user.save((err) => {
+                //Monogodb error checker
+                if (err) {
+                  res
+                    .status("400")
+                    .json({ message: "An error occurred", error: err.message });
+                  process.exit(1);
+                }
+                res.status("201").json({ message: "Update successful", user });
+              });
+            } else {
+              res.status(400).json({ message: "User is already an Admin" });
+            }
+          })
+          .catch((error) => {
+            res
+              .status(400)
+              .json({ message: "An error occurred", error: error.message });
+          });
+  
+        }
+    }
+}
   exports.deleteUser = async (req, res, next) => {
     const { id } = req.body
     await User.findById(id)
--- Auth/route.js
+++ Auth/route.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const router = express.Router()
-const { register, login, deleteUser } = require("./auth")
+const { register, login, update, deleteUser } = require("./auth")
 router.route("/register").post(register)
 router.route("/login").post(login);
+router.route("/update").put(update);
 router.route("/deleteUser").delete(deleteUser);
 module.exports = router
