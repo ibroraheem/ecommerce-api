940048fbc8a9ce5b5eae7e349f6bebdaaa63db00
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-06-06 16:03:31 +0100

    fixed

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const { register, login, update, deleteUser, displayUsers } = require("../Auth/auth")
+const { register, login, update, deleteUser, displayUsers } = require("../Auth/Auth")
 router.route("/register").post(register)
 router.route("/login").post(login);
 router.route("/update").put(update);
